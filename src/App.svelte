6d7ebe1843893f19797b1568d7cdf07c21130af9
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-05 16:57:32 +0530

    video

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<iframe width="560" height="315" src="https://www.youtube.com/embed/F9V3_beDxtg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</main>
+
+<style>
+
+</style>
